ba97bd855429a0a46c66c6fc3bbd3cb75e5b206f
Omer Wazir <omer.wazir@gmail.com>
2014-04-12 09:12:25 -0700

    Specific webjob for uploding css

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Webjobs Rock!!!');
+var azure = require('azure');
+var blobService = azure.createBlobService();
+var cssFile = '../../../../styles/style.css'
+
+blobService.putBlockBlobFromFile('css', 'omerwazir.css', cssFile, {contentType:'text/css'}, function(error) {
+  if(!error) {
+    console.log('omerwazir.css uploaded');
+  } else {
+    console.error('Error: ' + error);
+  }
+});
